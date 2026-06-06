d088d0466d956d5f3a68ad78972f92bdd3d84b52
Chen Kar Chun <ckc06chun@gmail.com>
2026-06-06 19:04:29 +0800

    fix: queue order placed notifications

--- app/Listeners/SendOrderNotification.php
+++ app/Listeners/SendOrderNotification.php
@@ -4,9 +4,12 @@ namespace App\Listeners;
 
 use App\Events\OrderPlaced;
 use App\Notifications\OrderPlacedNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendOrderNotification
+class SendOrderNotification implements ShouldQueue
 {
+    public bool $afterCommit = true;
+
     public function handle(OrderPlaced $event): void
     {
         $event->user->notify(new OrderPlacedNotification($event->order));
